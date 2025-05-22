a0415f9b600ea8c3d7db9d9e1ed6ff7d196d45e3
sai <saivenkat67687@gmail.com>
2025-05-22 21:35:49 +0530

    hello world started

---
+++ 03.variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+person1=trump
+person2=modi
+
+echo "$person1 :: hii rs gsndu "
+echo "$person2 :: cheppu macha em katta "
+echo "$person1 :: em bha pakistan ki chukalli chipincharu antta gha"
+echo "$person2  :: eddo ra bha edo alla russia maa macha putin help cheysadu lee"
