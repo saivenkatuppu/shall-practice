17ebbfffe1d77969a2466738f68f4cb65d56ded2
sai <saivenkat67687@gmail.com>
2025-05-24 00:34:06 +0530

    special var added

--- install-sql.sh
+++ install-sql.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
-USERID=$(id -u)
+# USERID=$(id -u)
 
-if [ $USERID -ne 0 ]
-then 
-    echo "ERROR : PLEASE UPDATE THE USER INTO ROOT"
+# if [ $USERID -ne 0 ]
+# then 
+#     echo "ERROR : PLEASE UPDATE THE USER INTO ROOT"
     
-else
-    echo "RUNNING WITH ROOT ACCESS"
-fi
+# else
+#     echo "RUNNING WITH ROOT ACCESS"
+# fi
+dnf list installed mysql
+if [ $? -ne 0 ]
+then
+echo "mysql is not installed going to install"
 dnf install mysql -y
+if [ $? -ne 0 ]
+then 
+echo "installing mysql is success"
+else
+echo "installing mysql is failure"
+fi
+else
+echo "my sql isalready in laptop"
