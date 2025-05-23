b6948fb2f740083ccf3636004ee472d0da9c91b4
sai <saivenkat67687@gmail.com>
2025-05-23 23:26:30 +0530

    special var added

--- conditions.sh
+++ conditions.sh
@@ -4,7 +4,7 @@ NUMBER1=$1
 
  if [ $NUMBER -lt 10 ]
  then 
-    echo "Given number $NUMBER is less than 10"
+    echo "Given number $NUMBER1 is less than 10"
  else
-   echo "Given number $NUMBER is greater than 10"
+   echo "Given number $NUMBER1 is greater than 10"
 fi
