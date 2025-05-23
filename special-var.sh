6f0230f416f3cd9fdb8b4cd9a91105111555cd55
sai <saivenkat67687@gmail.com>
2025-05-23 22:53:25 +0530

    special var added

--- special-var.sh
+++ special-var.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-echo "all var passed to the script: $@ "
-echo "Number of variables: $# "
+echo "all var passed to the script: $@"
+echo "Number of variables: $#"
+echo "script name : $@"
+echo "current directory :$PWD"
+echo "user running this script : $Home"
+echo "PID of the script: $$"
+sleep 10 &
+echo "PID of the last command in bg: $!"
