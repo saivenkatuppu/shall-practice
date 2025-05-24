aa0cc1a47992faf7fd7b5077940ee598d41a138e
sai <saivenkat67687@gmail.com>
2025-05-24 12:50:44 +0530

    practice ready

--- install-sql.sh
+++ install-sql.sh
@@ -9,11 +9,19 @@ then
 else    
     echo "you are a root user"
 fi
-dnf install mysql -y
+dnf list installed mysql
+if [ $? -ne 0 ]
+ then
+    echo "MY SQL IS not installed...goingto install it"
+    dnf install mysql -y
  if [ $? -eq 0 ]
  then 
     echo "installing mysql"
 else
     echo "installing my sql is failure"
-    exit 0
+    exit 1
+fi
+else 
+    echo "its already install"
+    exit 1
 fi
