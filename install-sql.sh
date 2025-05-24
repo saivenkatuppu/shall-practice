65c5c0e6f2b44f3071251224e02b78c351909389
sai <saivenkat67687@gmail.com>
2025-05-24 12:00:40 +0530

    practice ready

--- install-sql.sh
+++ install-sql.sh
@@ -1,25 +1,12 @@
 #!/bin/bash
 
-# USERID=$(id -u)
+USERID=$(id -u)
 
-# if [ $USERID -ne 0 ]
-# then 
-#     echo "ERROR : PLEASE UPDATE THE USER INTO ROOT"
-    
-# else
-#     echo "RUNNING WITH ROOT ACCESS"
-# fi
-dnf list installed mysql
-if [ $? -ne 0 ]
-then
-echo "mysql is not installed going to install"
-dnf install mysql -y
-if [ $? -eq 0 ]
+if [ $USERID -ne 0 ]
 then 
-echo "installing mysql is success"
-else
-echo "installing mysql is failure"
+    echo "error:: please run this script with root user"
+else    
+    echo "you are a root user"
 fi
-else
-echo "my sql isalready in laptop"
+dnf install mysql -y
 fi
