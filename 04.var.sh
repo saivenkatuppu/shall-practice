a3922fd20134bdb7da157d0b527382717e81c6fe
sai <saivenkat67687@gmail.com>
2025-05-25 15:30:34 +0530

    hello world started

--- 04.var.sh
+++ 04.var.sh
@@ -5,4 +5,4 @@ person2=$2
 echo "$person1 :: hii rs gsndu "
 echo "$person2 :: cheppu macha em katta "
 echo "$person1 :: em bha pakistan ki chukalli chipincharu antta gha"
-echo "$person2 :: eddo ra bha edo alla russia maa macha putin help cheysadu lee"
+echo "$person2 :: eddo ra bha edo alla russia maa macha putin help cheysadu lee" 
