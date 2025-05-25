0c03eefd9bfab152fe26012d91f27822e77cf2d5
sai <saivenkat67687@gmail.com>
2025-05-25 16:06:33 +0530

    hello world started

--- datatypesss.sh
+++ datatypesss.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 NUMBER1=$1
 NUMBER2=$2
+NUMBER2=$3
 
 TIMESTAMP=$(date)
 echo "script exectue date at : $TIMESTAMP"
-SUM=$(($1+$2))
+SUM=$(($1+$2+$3))
 
 echo "SUM of $1 and $2 is :$SUM"
