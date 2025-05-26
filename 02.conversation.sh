a28a532a646181ab32bb3d22b7efa9c21eaef769
sai <saivenkat67687@gmail.com>
2025-05-26 22:09:57 +0530

    shall-practice

--- 02.conversation.sh
+++ 02.conversation.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-echo -e "/e[31m trumph :: hii rs gsndu "
-echo -e "/e[32m modi :: cheppu macha em katta "
-echo -e "/e[33m trumph :: em bha pakistan ki chukalli chipincharu antta gha"
-echo -e "/e[34m modi  :: eddo ra bha edo alla russia maa macha putin help cheysadu lee "
-echo -e "/e[35m modi  :: eddo ra bha edo alla russia maa macha putin help cheysadu lee "
-echo -e "/e[36m modi  :: eddo ra bha edo alla russia maa macha putin help cheysadu lee "
-echo -e "/e[37m modi  :: eddo ra bha edo alla russia maa macha putin help cheysadu lee "
-echo -e "/e[0 mmodi  :: eddo ra bha edo alla russia maa macha putin help cheysadu lee "
+echo -e "\e[31m trumph :: hii rs gsndu "
+echo -e "\e[32m modi :: cheppu macha em katta "
+echo -e "\e[33m trumph :: em bha pakistan ki chukalli chipincharu antta gha"
+echo -e "\e[34m modi  :: eddo ra bha edo alla russia maa macha putin help cheysadu lee "
+echo -e "\e[35m modi  :: eddo ra bha edo alla russia maa macha putin help cheysadu lee "
+echo -e "\e[36m modi  :: eddo ra bha edo alla russia maa macha putin help cheysadu lee "
+echo -e "\e[37m modi  :: eddo ra bha edo alla russia maa macha putin help cheysadu lee "
+echo -e "\e[0 mmodi  :: eddo ra bha edo alla russia maa macha putin help cheysadu lee "
